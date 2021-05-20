08161735e6d265522105556c5ab3489d21155b15
LeenJooken <leen.jooken@hotmail.com>
2021-05-20 14:39:07 +0200

    commit 3 on main
    okijhgfc

--- codefile3.py
+++ codefile3.py
@@ -7,8 +7,8 @@ parser.add_argument("-ef","--edges_filename",help="pass the edges CSV filename")
 
 args = parser.parse_args()
 log_file = args.source
-nodes_file = args.nodes_filename
-edges_file = args.edges_filename
+nodes_file = args.nodes_fi
+edges_file = args.edges_fi
 
 #run the program
 main(log_file,nodes_file,edges_file)
