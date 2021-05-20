59f9e4495f838ec7048510b127a251bfdef4328e
LeenJooken <leen.jooken@hotmail.com>
2021-05-20 16:56:10 +0200

    rename arg
    Co-Authored-By: Greg VAN HOUDT <55534825+gregvanhoudt@users.noreply.github.com>

--- codefile3.py
+++ codefile3.py
@@ -1,14 +1,14 @@
 #parse the command line arguments
 print("Parsing the arguments")
 parser = argparse.ArgumentParser()
-parser.add_argument("-s","--source",help="pass the sourcefile to be read")
-parser.add_argument("-nf","--nodes_filename",help="pass the nodes CSV filename")
+parser.add_argument("-s","--source",help="pass the gfcxghuiop to be read")
+parser.add_argument("-nf","--jjjj",help="pass the nodes CSV filename")
 parser.add_argument("-ef","--edges_filename",help="pass the edges CSV filename")
 
 args = parser.parse_args()
 log_file = args.source
-nodes_file = args.nodes_fi
-edges_file = args.edges_fi
+nodes_file = args.nodes_file
+edges_file = args.edges_file
 
 #run the program
 main(log_file,nodes_file,edges_file)
